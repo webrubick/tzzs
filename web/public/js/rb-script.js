2d05b01581a513a05645511dfc13620c5c41196c
Yin Yong <yy15151877621@126.com>
2016-02-24 19:16:14 +0800

    首页主框架定型

--- web/public/js/rb-script.js
+++ web/public/js/rb-script.js
@@ -47,17 +47,42 @@ function isIE() {
  */
 ;
 (function ($) {
+    // 竖向的引线
+    var homeContentBefore = $('.vline');
+    var lineHeight = 30;
+    
+    var mains = $('main');
+    var mainVPadding = 25;
+    
+    // 首页品牌的界面
 	var brand = $('.brand');
-	function updateMinHeight() {
+	var brandMinHeight = 372 + 160 + 50;
+	brand.css({
+	    'padding-top': '160px',
+	    'padding-bottom': '50px',
+	});
+	function update() {
 		var n = $(window).height();
-    	n = Math.max(n, 532);
+    	n = Math.max(n, brandMinHeight);
+    	var m = Math.min(n - brandMinHeight + lineHeight, 100);
 		brand.css({
 			'min-height' : n + 'px',
 		});
+		
+		homeContentBefore.css({
+			'top' : (-m) + 'px',
+			'height' : (m * 2) + 'px',
+		});
+		
+		var o = (mainVPadding + m) + 'px';
+		mains.css({
+		    'padding-top': o,
+		    'padding-bottom': o,
+		});
 	}
-	updateMinHeight();
+	update();
 	$(window).resize(function() {
-    	updateMinHeight();
+    	update();
 	});
 })(jQuery);
 
