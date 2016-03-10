0e7399b721e9f4f9d93f5b44650ac9007cc9a50f
Yin Yong <yy15151877621@126.com>
2016-03-10 17:10:53 +0800

    界面微调

--- application/views/case-main.php
+++ application/views/case-main.php
@@ -3,9 +3,9 @@ $extra_head[] = title_tag($website['website_name']);
 $extra_head[] = exlink_tag('public/css/index.css');
 ?>
 
-<?php $this->load->view('common/page-top.php', array('extra_head' => $extra_head)); ?>
+<?php $this->load->view('common/page-top', array('extra_head' => $extra_head)); ?>
 
-<?php $this->load->view('common/page-header.php', array('extra_header' => array())); ?>
+<?php $this->load->view('common/page-header', array('extra_header' => array())); ?>
         
         <!-- main content -->
         <section class="content-wrap">
@@ -15,11 +15,11 @@ $extra_head[] = exlink_tag('public/css/index.css');
                 </main>
             </section>
         
-    <?php $this->load->view('common/page-footer.php'); ?>
+    <?php $this->load->view('common/page-footer'); ?>
 
         </section>
         
         <?php echo script_tag('public/js/core.min.js'); ?>
         <?php echo script_tag('public/js/rb-script.js'); ?>
         
-<?php $this->load->view('common/page-bottom.php'); ?>
+<?php $this->load->view('common/page-bottom'); ?>
---
+++ application/views/templates/template-brand-aspiration.php
@@ -0,0 +1,16 @@
+                
+                <span class="hint-aspiration">
+                    <ul>
+                    	<li>品</li>
+                    	<li>质</li>
+                    	<li>设</li>
+                    	<li>计</li>
+                    	&nbsp;&nbsp;
+                    	<li>服</li>
+                    	<li>务</li>
+                    	<li>至</li>
+                    	<li>上</li>
+                    </ul>
+                	<!-- 品质设计&nbsp;&nbsp;服务至上 -->
+                </span>
+                
