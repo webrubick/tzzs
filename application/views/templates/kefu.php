75e869af0d423c957d66653b4ec4f55458a0a0de
Yin Yong <yy15151877621@126.com>
2016-04-07 21:40:23 +0800

    TQ客服系统接入

--- application/views/templates/kefu.php
+++ application/views/templates/kefu.php
@@ -1,3 +1,2 @@
 
-<a id="ibangkf" href="http://www.ibangkf.com">在线客服</a>
-<script type="text/javascript" src="http://c.ibangkf.com/i/c-bblue000.js"></script>
+<script language="JavaScript" src="http://float2006.tq.cn/floatcard?adminid=9724986&sort=0" ></script>
